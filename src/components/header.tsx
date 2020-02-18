315a33c463fcd5433b8820788132f050a44706d3
Rafa Gomes <rafaelgomes.web@gmail.com>
2020-02-18 00:53:56 -0300

    Added emotion

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,34 +2,24 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import styled from '@emotion/styled';
+
+const StyledLink = styled(props => <Link {...props} />)`
+  color: black;
+  text-decoration: none;
+`;
+
 interface Props {
   siteTitle: string
 }
 
 const Header = ({ siteTitle }:Props) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header>
+    <div>
+      <h1>
+        <StyledLink to="/">
           {siteTitle}
-        </Link>
+        </StyledLink>
       </h1>
     </div>
   </header>
